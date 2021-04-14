5364f5d20e29ea2eccc83a3a36197b0af4cbad92
Solo-Dragon <77414846+Solo-Dragon@users.noreply.github.com>
2021-04-14 16:13:08 +0530

    Create AI_Chat.py

---
+++ tg_bot/modules/AI_Chat.py
@@ -0,0 +1,325 @@
+import re
+import emoji
+import requests
+url = "https://iamai.p.rapidapi.com/ask"
+from tg_bot import OWNER_ID, BOT_ID
+from tg_bot.services.pyrogram import pbot as tg_bot
+from pyrogram import filters
+import asyncio, os
+from tg_bot.function.pluginhelpers import admins_only
+from json import JSONDecodeError
+import json
+from google_trans_new import google_translator
+translator = google_translator()
+def extract_emojis(s):
+    return "".join(c for c in s if c in emoji.UNICODE_EMOJI)
+daisy_chats = []
+en_chats = []
+# AI Chat (C) 2020-2021 by @InukaAsith
+
+@daisyx.on_message(filters.command("chatbot") & ~filters.edited & ~filters.bot)
+@admins_only
+async def hmm(_, message):
+    global daisy_chats
+    if len(message.command) != 2:
+        await message.reply_text("I only recognize `/chatbot on` and /chatbot `off only`")
+        message.continue_propagation()
+    status = message.text.split(None, 1)[1] 
+    chat_id = message.chat.id
+    if status == "ON" or status == "on" or status == "On":
+        if chat_id not in daisy_chats:
+            if chat_id in en_chats:
+                en_chats.remove(chat_id)
+            daisy_chats.append(message.chat.id)
+            text = "Chatbot Enabled Reply To Any Message" \
+                   + "Of Jarvis to Get A Reply"
+            await message.reply_text(text)
+            message.continue_propagation()
+        await message.reply_text("ChatBot Is Already Enabled.")
+        message.continue_propagation()
+
+    elif status == "OFF" or status == "off" or status == "Off":
+        if chat_id in daisy_chats:
+            if chat_id in en_chats:
+                en_chats.remove(chat_id)
+            daisy_chats.remove(chat_id)
+            await message.reply_text("AI chat Disabled!")
+            return
+        await message.reply_text("AI Chat Is Already Disabled.")
+        message.continue_propagation()
+    elif status == "EN" or status == "en" or status == "english":
+        if not chat_id in en_chats:
+            en_chats.append(chat_id)
+            await message.reply_text("English AI chat Enabled!")
+            return
+        await message.reply_text("AI Chat Is Already Disabled.")
+        message.continue_propagation()
+    else:
+        await message.reply_text("I only recognize `/chatbot on` and /chatbot `off only`")
+
+
+
+@daisyx.on_message(filters.text & filters.reply & ~filters.bot &
+        ~filters.via_bot & ~filters.forwarded ,group=2)
+async def hmm(client,message):
+  if message.chat.id not in daisy_chats:
+    message.continue_propagation()
+  if message.reply_to_message.from_user.id != BOT_ID:
+    message.continue_propagation()
+  msg = message.text
+  chat_id = message.chat.id
+  if msg.startswith("/") or msg.startswith("@"):
+    message.continue_propagation()
+  if chat_id in en_chats:
+    test = msg
+    test = test.replace('Jarvis', 'Jessica')
+    test = test.replace('Jarvis', 'Jessica')
+    r = ('\n    \"consent\": true,\n    \"ip\": \"::1\",\n    \"question\": \"{}\"\n').format(test)
+    k = f"({r})"
+    new_string = k.replace("(", "{")
+    lol = new_string.replace(")","}")
+    payload = lol
+    headers = {
+        'content-type': "application/json",
+        'x-forwarded-for': "<user's ip>",
+        'x-rapidapi-key': "33b8b1a671msh1c579ad878d8881p173811jsn6e5d3337e4fc",
+        'x-rapidapi-host': "iamai.p.rapidapi.com"
+        }
+ 
+    response = requests.request("POST", url, data=payload, headers=headers)
+    lodu = response.json()
+    result = (lodu['message']['text'])
+    pro = result
+    pro = pro.replace('Thergiakis Eftichios','Indrajeeth')
+    pro = pro.replace('Jessica','Jarvis')
+    if "Out of all ninja turtle" in result:
+     pro = "Sorry! looks I missed that. I'm at your service ask anthing sir?"
+    if "ann" in result:
+     pro = "My name is Jarvis"
+    try:
+      await daisyx.send_chat_action(message.chat.id, "typing")
+      await message.reply_text(pro)
+    except CFError as e:
+           print(e)
+  else:
+    u = msg.split()
+    emj = extract_emojis(msg)
+    msg = msg.replace(emj, "")
+    if (      
+        [(k) for k in u if k.startswith("@")]
+        and [(k) for k in u if k.startswith("#")]
+        and [(k) for k in u if k.startswith("/")]
+        and re.findall(r"\[([^]]+)]\(\s*([^)]+)\s*\)", msg) != []
+  ):
+    
+      h = " ".join(filter(lambda x: x[0] != "@", u))
+      km = re.sub(r"\[([^]]+)]\(\s*([^)]+)\s*\)", r"", h)
+      tm = km.split()
+      jm = " ".join(filter(lambda x: x[0] != "#", tm))
+      hm = jm.split()
+      rm = " ".join(filter(lambda x: x[0] != "/", hm))
+    elif [(k) for k in u if k.startswith("@")]:
+    
+      rm = " ".join(filter(lambda x: x[0] != "@", u))
+    elif [(k) for k in u if k.startswith("#")]:
+      rm = " ".join(filter(lambda x: x[0] != "#", u))
+    elif [(k) for k in u if k.startswith("/")]:
+      rm = " ".join(filter(lambda x: x[0] != "/", u))
+    elif re.findall(r"\[([^]]+)]\(\s*([^)]+)\s*\)", msg) != []:
+      rm = re.sub(r"\[([^]]+)]\(\s*([^)]+)\s*\)", r"", msg)
+    else:
+      rm = msg
+      #print (rm)
+      lan = translator.detect(rm)
+    test = rm
+    if not "en" in lan and not lan == "":
+      test = translator.translate(test, lang_tgt="en")
+        
+  #test = emoji.demojize(test.strip())
+  
+# Kang with the credits bitches @InukaASiTH
+
+
+    test = test.replace('jarvis', 'Jessica')
+    test = test.replace('Jarvis', 'Jessica')
+    r = ('\n    \"consent\": true,\n    \"ip\": \"::1\",\n    \"question\": \"{}\"\n').format(test)
+    k = f"({r})"
+    new_string = k.replace("(", "{")
+    lol = new_string.replace(")","}")
+    payload = lol
+    headers = {
+        'content-type': "application/json",
+        'x-forwarded-for': "<user's ip>",
+        'x-rapidapi-key': "33b8b1a671msh1c579ad878d8881p173811jsn6e5d3337e4fc",
+        'x-rapidapi-host': "iamai.p.rapidapi.com"
+        }
+ 
+    response = requests.request("POST", url, data=payload, headers=headers)
+    lodu = response.json()
+    result = (lodu['message']['text'])
+    pro = result
+    pro = pro.replace('Thergiakis Eftichios','Indrajeeth')
+    pro = pro.replace('Jessica','Jarvis')
+    if "Out of all ninja turtle" in result:
+     pro = "Sorry! looks I missed that. I'm at your service ask anthing sir?"
+    if "ann" in result:
+     pro = "My name is Jarvis"
+    if not "en" in lan and not lan == "":
+      pro = translator.translate(pro, lang_tgt=lan[0])
+    try:
+      await daisyx.send_chat_action(message.chat.id, "typing")
+      await message.reply_text(pro)
+    except CFError as e:
+           print(e)
+  
+
+@daisyx.on_message(filters.text & filters.private & filters.reply & ~filters.bot)
+async def inuka(client,message):
+  msg = message.text
+  if msg.startswith("/") or msg.startswith("@"):
+    message.continue_propagation()
+  u = msg.split()
+  emj = extract_emojis(msg)
+  msg = msg.replace(emj, "")
+  if (      
+      [(k) for k in u if k.startswith("@")]
+      and [(k) for k in u if k.startswith("#")]
+      and [(k) for k in u if k.startswith("/")]
+      and re.findall(r"\[([^]]+)]\(\s*([^)]+)\s*\)", msg) != []
+):
+    
+    h = " ".join(filter(lambda x: x[0] != "@", u))
+    km = re.sub(r"\[([^]]+)]\(\s*([^)]+)\s*\)", r"", h)
+    tm = km.split()
+    jm = " ".join(filter(lambda x: x[0] != "#", tm))
+    hm = jm.split()
+    rm = " ".join(filter(lambda x: x[0] != "/", hm))
+  elif [(k) for k in u if k.startswith("@")]:
+    
+    rm = " ".join(filter(lambda x: x[0] != "@", u))
+  elif [(k) for k in u if k.startswith("#")]:
+    rm = " ".join(filter(lambda x: x[0] != "#", u))
+  elif [(k) for k in u if k.startswith("/")]:
+    rm = " ".join(filter(lambda x: x[0] != "/", u))
+  elif re.findall(r"\[([^]]+)]\(\s*([^)]+)\s*\)", msg) != []:
+    rm = re.sub(r"\[([^]]+)]\(\s*([^)]+)\s*\)", r"", msg)
+  else:
+    rm = msg
+    #print (rm)
+    lan = translator.detect(rm)
+  test = rm
+  if not "en" in lan and not lan == "":
+    test = translator.translate(test, lang_tgt="en")
+        
+  #test = emoji.demojize(test.strip())
+  
+# Kang with the credits bitches @InukaASiTH
+
+
+  test = test.replace('Jarvis', 'Jessica')
+  test = test.replace('Jarvis', 'Jessica')
+  r = ('\n    \"consent\": true,\n    \"ip\": \"::1\",\n    \"question\": \"{}\"\n').format(test)
+  k = f"({r})"
+  new_string = k.replace("(", "{")
+  lol = new_string.replace(")","}")
+  payload = lol
+  headers = {
+      'content-type': "application/json",
+      'x-forwarded-for': "<user's ip>",
+      'x-rapidapi-key': "33b8b1a671msh1c579ad878d8881p173811jsn6e5d3337e4fc",
+      'x-rapidapi-host': "iamai.p.rapidapi.com"
+      }
+ 
+  response = requests.request("POST", url, data=payload, headers=headers)
+  lodu = response.json()
+  result = (lodu['message']['text'])
+  pro = result
+  pro = pro.replace('Thergiakis Eftichios','Indrajeeth')
+  pro = pro.replace('Jessica','Jarvis')
+  if "Out of all ninja turtle" in result:
+   pro = "Sorry! looks I missed that. I'm at your service ask anthing sir?"
+  if "ann" in result:
+   pro = "My name is Jarvis"
+  if not "en" in lan and not lan == "":
+    pro = translator.translate(pro, lang_tgt=lan[0])
+  try:
+    await daisyx.send_chat_action(message.chat.id, "typing")
+    await message.reply_text(pro)
+  except CFError as e:
+         print(e)
+
+@daisyx.on_message(filters.regex("Jarvis|jarvis|Darkjarvis|darkjarvis") & ~filters.bot &
+        ~filters.via_bot & ~filters.forwarded & ~filters.reply & ~filters.channel)
+async def inuka(client,message):
+  msg = message.text
+  if msg.startswith("/") or msg.startswith("@"):
+    message.continue_propagation()
+  u = msg.split()
+  emj = extract_emojis(msg)
+  msg = msg.replace(emj, "")
+  if (      
+      [(k) for k in u if k.startswith("@")]
+      and [(k) for k in u if k.startswith("#")]
+      and [(k) for k in u if k.startswith("/")]
+      and re.findall(r"\[([^]]+)]\(\s*([^)]+)\s*\)", msg) != []
+):
+    
+    h = " ".join(filter(lambda x: x[0] != "@", u))
+    km = re.sub(r"\[([^]]+)]\(\s*([^)]+)\s*\)", r"", h)
+    tm = km.split()
+    jm = " ".join(filter(lambda x: x[0] != "#", tm))
+    hm = jm.split()
+    rm = " ".join(filter(lambda x: x[0] != "/", hm))
+  elif [(k) for k in u if k.startswith("@")]:
+    
+    rm = " ".join(filter(lambda x: x[0] != "@", u))
+  elif [(k) for k in u if k.startswith("#")]:
+    rm = " ".join(filter(lambda x: x[0] != "#", u))
+  elif [(k) for k in u if k.startswith("/")]:
+    rm = " ".join(filter(lambda x: x[0] != "/", u))
+  elif re.findall(r"\[([^]]+)]\(\s*([^)]+)\s*\)", msg) != []:
+    rm = re.sub(r"\[([^]]+)]\(\s*([^)]+)\s*\)", r"", msg)
+  else:
+    rm = msg
+    #print (rm)
+    lan = translator.detect(rm)
+  test = rm
+  if not "en" in lan and not lan == "":
+    test = translator.translate(test, lang_tgt="en")
+        
+  #test = emoji.demojize(test.strip())
+  
+# Kang with the credits bitches @InukaASiTH
+
+
+  test = test.replace('jarvis', 'Jessica')
+  test = test.replace('Jarvis', 'Jessica')
+  r = ('\n    \"consent\": true,\n    \"ip\": \"::1\",\n    \"question\": \"{}\"\n').format(test)
+  k = f"({r})"
+  new_string = k.replace("(", "{")
+  lol = new_string.replace(")","}")
+  payload = lol
+  headers = {
+      'content-type': "application/json",
+      'x-forwarded-for': "<user's ip>",
+      'x-rapidapi-key': "33b8b1a671msh1c579ad878d8881p173811jsn6e5d3337e4fc",
+      'x-rapidapi-host': "iamai.p.rapidapi.com"
+      }
+ 
+  response = requests.request("POST", url, data=payload, headers=headers)
+  lodu = response.json()
+  result = (lodu['message']['text'])
+  pro = result
+  pro = pro.replace('Thergiakis Eftichios','Indrajeeth')
+  pro = pro.replace('Jessica','Jarvis')
+  if "Out of all ninja turtle" in result:
+   pro = "Sorry! looks I missed that. I'm at your service ask anthing sir?"
+  if "ann" in result:
+   pro = "My name is Jarvis"
+  if not "en" in lan and not lan == "":
+    pro = translator.translate(pro, lang_tgt=lan[0])
+  try:
+    await daisyx.send_chat_action(message.chat.id, "typing")
+    await message.reply_text(pro)
+  except CFError as e:
+         print(e)
